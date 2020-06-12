4e6d5be902d4b082671b5cfe3e84a6b4d5284a7e
xltxlm <xltxlm@qq.com>
2020-06-12 14:05:00 +0800

    message:(001271)

--- test/Arr/getbyIndex_234_0_test/getbyIndex_234_0Provider.go
+++ test/Arr/getbyIndex_234_0_test/getbyIndex_234_0Provider.go
@@ -15,41 +15,49 @@ func  DataProvider() []interface{} {
             []string{"a","b","d"},
             "b",
             1,
+            "获取第一个索引 - id=74",
         },
         []interface{}{
             []string{"a","b","e"},
             "e",
             2,
+            " - id=75",
         },
         []interface{}{
             []string{"a","b","e"},
             "a",
             0,
+            " - id=76",
         },
         []interface{}{
             []string{"a","b","e"},
             nil,
             4,
+            "要获取的索引超出的数组的长度，返回nil - id=77",
         },
         []interface{}{
             []string{"a","b","e"},
             "b",
             -2,
+            " - id=78",
         },
         []interface{}{
             []string{"a","b","e"},
             "e",
             -1,
+            " - id=79",
         },
         []interface{}{
             []interface{}{"a","b","e"},
             "e",
             -1,
+            " - id=80",
         },
         []interface{}{
             []interface{}{"a","b","e",goarray.Arr{}},
             goarray.Arr{},
             -1,
+            " - id=81",
         },
 	}
 
